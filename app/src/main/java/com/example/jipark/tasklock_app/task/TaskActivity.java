ba6f3deacb425e0063cc3e87b019eecbe6ace1fc
jihoonpark4829@gmail.com <jihoonpark4829@gmail.com>
2017-10-04 22:12:47 -0600

    list adapter

--- app/src/main/java/com/example/jipark/tasklock_app/task/TaskActivity.java
+++ app/src/main/java/com/example/jipark/tasklock_app/task/TaskActivity.java
@@ -2,6 +2,7 @@ package com.example.jipark.tasklock_app.task;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.jipark.tasklock_app.R;
 import com.example.jipark.tasklock_app.Task;
@@ -19,7 +20,12 @@ public class TaskActivity extends AppCompatActivity {
         setContentView(R.layout.activity_task);
     }
 
-    //Reading and writing data
+    public void finishTaskActivity(View view) {
+        saveTasks();
+        finish();
+    }
+
+    //reading and writing data
     private String loadJSON() {
         String jsonOutput = null;
         try {
@@ -87,3 +93,34 @@ public class TaskActivity extends AppCompatActivity {
 //        } catch (JSONException e) {
 //        e.printStackTrace();
 //        }
+
+
+
+
+
+//public class ListViewDemo extends ListActivity {
+//    //LIST OF ARRAY STRINGS WHICH WILL SERVE AS LIST ITEMS
+//    ArrayList<String> listItems=new ArrayList<String>();
+//
+//    //DEFINING A STRING ADAPTER WHICH WILL HANDLE THE DATA OF THE LISTVIEW
+//    ArrayAdapter<String> adapter;
+//
+//    //RECORDING HOW MANY TIMES THE BUTTON HAS BEEN CLICKED
+//    int clickCounter=0;
+//
+//    @Override
+//    public void onCreate(Bundle icicle) {
+//        super.onCreate(icicle);
+//        setContentView(R.layout.main);
+//        adapter=new ArrayAdapter<String>(this,
+//                android.R.layout.simple_list_item_1,
+//                listItems);
+//        setListAdapter(adapter);
+//    }
+//
+//    //METHOD WHICH WILL HANDLE DYNAMIC INSERTION
+//    public void addItems(View v) {
+//        listItems.add("Clicked : "+clickCounter++);
+//        adapter.notifyDataSetChanged();
+//    }
+//}
