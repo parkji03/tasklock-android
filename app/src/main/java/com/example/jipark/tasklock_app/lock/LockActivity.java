0e517732d07b208aea5e34747dcf823a64a7d3c3
jihoonpark4829@gmail.com <jihoonpark4829@gmail.com>
2017-10-11 13:06:15 -0600

    lock act

--- app/src/main/java/com/example/jipark/tasklock_app/lock/LockActivity.java
+++ app/src/main/java/com/example/jipark/tasklock_app/lock/LockActivity.java
@@ -12,4 +12,15 @@ public class LockActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lock);
     }
+
+
+    /* TODO:    always have this display on
+       TODO:    show a list of tasks
+       TODO:    have check mark boxes to each task
+       TODO:    on completion, all done, return to main activity
+            check with a loop every time the user checks a check mark, check how many is left, and if 0, we're done
+
+
+    */
+
 }
