b7b0b53e7f8ee1d33cd8cd78cc92cd18aaa3588e
SCOTT-LAPTOP\Scott <scottchristensen111@gmail.com>
2017-10-16 17:34:28 -0600

    App List display, nothing else.

--- app/src/main/java/com/example/jipark/tasklock_app/app_manager/AppManagerActivity.java
+++ app/src/main/java/com/example/jipark/tasklock_app/app_manager/AppManagerActivity.java
@@ -11,38 +11,60 @@ import com.example.jipark.tasklock_app.R;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
 public class AppManagerActivity extends AppCompatActivity {
     private PackageManager pm;
     private List<ApplicationInfo> apps;
     private List<ApplicationInfo> userApps = new ArrayList<>();
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private List<String> labelList = new ArrayList();
+    private List<Drawable> iconList = new ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_manager);
 
-//        pm = getPackageManager();
-//        apps = pm.getInstalledApplications(0);
+        pm = getPackageManager();
+        apps = pm.getInstalledApplications(0);
 
+        loadUserApplications();
     }
 
-//    public void loadUserApplications(){
-//        for(ApplicationInfo app : apps) {
-//            //checks for flags; if flagged, check if updated system app
-//            if((app.flags & ApplicationInfo.FLAG_UPDATED_SYSTEM_APP) != 0) {
-//                userApps.add(app);
-//                //it's a system app, not interested
-//            } else if ((app.flags & ApplicationInfo.FLAG_SYSTEM) != 0) {
-//                //Discard this one
-//                //in this case, it should be a user-installed app
-//            } else {
-//                userApps.add(app);
-//            }
-//        }
+    public void loadUserApplications(){
 
-//        String label = (String)pm.getApplicationLabel(app);
-//        Drawable icon = pm.getApplicationIcon(app);
-//    }
+        //create a recycler view for the labels and icons
+        recyclerView = (RecyclerView)findViewById(R.id.app_manager_list);
+        layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+
+
+        for(ApplicationInfo app : apps) {
+            //checks for flags; if flagged, check if updated system app
+            if ((app.flags & ApplicationInfo.FLAG_UPDATED_SYSTEM_APP) != 0) {
+                userApps.add(app);
+                //it's a system app, not interested
+            } else if ((app.flags & ApplicationInfo.FLAG_SYSTEM) != 0) {
+                //Discard this one
+                //in this case, it should be a user-installed app
+            } else {
+                userApps.add(app);
+            }
+
+            String label = (String) pm.getApplicationLabel(app);
+            Drawable icon = pm.getApplicationIcon(app);
+            labelList.add(label);
+            iconList.add(icon);
+        }
+
+        adapter = new AppManagerAdapter(labelList, iconList);
+        recyclerView.setAdapter(adapter);
+    }
 
 
     //TODO: https://stackoverflow.com/questions/36261909/how-to-make-app-lock-app-in-android
---
+++ app/src/main/java/com/example/jipark/tasklock_app/app_manager/AppManagerAdapter.java
@@ -0,0 +1,57 @@
+package com.example.jipark.tasklock_app.app_manager;
+
+/**
+ * Created by Scott on 10/13/2017.
+ */
+import android.graphics.drawable.Drawable;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.example.jipark.tasklock_app.R;
+import android.widget.ImageView;
+
+public class AppManagerAdapter extends RecyclerView.Adapter<AppManagerAdapter.AppManagerHolder> {
+
+    List<String> labelList;
+    List<Drawable> iconList;
+
+    public  AppManagerAdapter(List<String> labelList, List<Drawable> iconList) {
+        this.labelList = labelList;
+        this.iconList = iconList;
+    }
+
+    @Override
+    public  AppManagerAdapter.AppManagerHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v= LayoutInflater.from(parent.getContext()).inflate(R.layout.app_manager_row,parent,false);
+        AppManagerHolder viewHolder = new  AppManagerHolder(v);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(AppManagerAdapter.AppManagerHolder holder, int position) {
+        holder.text.setText(labelList.get(position).toString());
+        holder.image.setImageDrawable(iconList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return labelList.size();
+    }
+
+    public static class AppManagerHolder extends RecyclerView.ViewHolder{
+
+        protected TextView text;
+        protected ImageView image;
+
+        public AppManagerHolder(View itemView) {
+            super(itemView);
+            text = (TextView) itemView.findViewById(R.id.text_id);
+            image = (ImageView) itemView.findViewById(R.id.image_id);
+        }
+    }
+}
