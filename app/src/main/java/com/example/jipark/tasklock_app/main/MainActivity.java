7a4623675f3eebccb199a4565e3f69f02339e067
jihoonpark4829@gmail.com <jihoonpark4829@gmail.com>
2017-10-17 00:51:10 -0600

    iris mode

--- app/src/main/java/com/example/jipark/tasklock_app/main/MainActivity.java
+++ app/src/main/java/com/example/jipark/tasklock_app/main/MainActivity.java
@@ -5,6 +5,9 @@ import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -82,6 +85,27 @@ public class MainActivity extends AppCompatActivity {
             SINGLETON.loadTasks(this);
         }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+//        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_eye:
+                Toast.makeText(this, "IRIS MODE", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
 
     @Override
     protected void onDestroy() {
